7e2206dcebff247d782e78396dbfca6ca3ffbef5
PratikC-12 <pratik10.kop@gmail.com>
2025-05-14 13:37:05 +0530

    login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
-import Login from "./Auth/Login";
 import MemoHookParent from "./React Pratice Stuff/MemoHookParent";
 import Header from "./Header Files/Header";
 import Task from "./React Task/Task";
 import ArrayTask from "./React Pratice Stuff/ArrayTask";
 import If_Else_CheckingTax from "./React Pratice Stuff/If_Else_CheckingTax";
 import SortTaskByArray from "./React Pratice Stuff/SortTaskByArray";
+import Login from "./Auth/Login";
 
 const App = () => {
   return (
